dff75da102155c6c6b3476914e580dbc80793537
Your Name <prashantsinghberath@gmail.com>
2026-08-21 17:15:17 +0530

    python_3.13

---
+++ code.py
@@ -0,0 +1,96 @@
+# ------ OUTPUT -------
+
+# print("Hello Python-3.13")
+
+# ----- VARIABLES -----
+
+# name="Prashant"
+# age=22
+# print(name, age)
+# name="Shubham"
+# age=23
+
+# ------- TYPES OF ------
+
+# cgpa=8.1 
+# print(name,age)
+# print(type(age))
+# print(type(name))
+# print(type(cgpa))
+
+# ------ USER INPUT -------
+
+# name= input("Enter your name: ")
+# print("Your name is",name)
+# print("hello " + name) --> # Concatenation
+
+# ------ TYPE CONVERSION & TYPE CASTING -------
+
+# age= input("Enter your age: ")
+# print(age)
+# new_age= int(age)+1
+# print(new_age)
+# print(float(new_age))
+# print(bool(new_age))
+# print(str(new_age))
+
+# ---- SUM PROGRAM -----
+
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# sum=a+b
+# print("Sum:",sum)
+
+# ------ STRING --------
+
+# upper & lower case
+
+# name="Prashant"
+# print(name.upper())
+# print(name.lower())
+# print(name)
+
+# ----- STRING OPERATIONS ------
+
+# find
+
+# name="Prashant"
+# print(name.find("ant"))
+# print(name.find("a"))
+# print(name.find("z"))
+
+# replace
+
+# print(name.replace("Prashant","Prashi"))
+# print(name.replace("ant","5"))
+
+# check for presence
+# name="Prashant"
+# print('v' in name)
+# print('t' in name)
+
+# --------- ARITHMETIC OPERATORS -----------
+
+# print(5+31)
+# print(31-5)
+# print(5*31)
+# print(31/5)
+# print(31//5)
+# print(31%5)
+# print(31**5)
+
+# ------- CONDITIONAL STATEMENTS --------
+# indentation
+# age=24
+# if age>18:
+#     print("You r adult")
+#     print("You can vote/drive")
+
+# print("End of code") 
+
+
+
+
+
+
+
